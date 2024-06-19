408fb47df42340eee92a011f74b81f4338868d42
oriyaPerel <159001341+oriyaPerel@users.noreply.github.com>
2024-06-19 22:44:11 +0300

    Update node.hpp

--- node.hpp
+++ node.hpp
@@ -10,14 +10,15 @@ class Node
 
 private:
     T value;
-    std::vector<Node<T>*> children;
+    std::vector<Node<T> *> children;
+
 public:
     Node(T value) : value(value) {}
+    ~Node() {}
+    // ~Node(){ for (auto child : children) delete child; }
     T get_value() const { return value; }
     void add_child(Node<T> *child) { children.push_back(child); }
-    std::vector<Node<T>*> get_children() const { return children; }
-    
+    std::vector<Node<T> *> get_children() const { return children; }
 };
 
 #endif
-
